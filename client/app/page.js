cb65d539e2d405753b4bf6d85b85f14c7875d83b
Raman01010010 <rmnprjrrr@gmail.com>
2023-12-27 08:44:20 +0530

    next project initialized

---
+++ client/app/page.js
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+   <section className='w-full flex-center flex-col'>
+<h1 className='text-center'>Discover & Share</h1>
+   </section>
+  )
+}
+
+export default page
