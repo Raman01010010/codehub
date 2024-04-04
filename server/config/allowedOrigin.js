7a29b2445c72879b1f9cae2c1fb8974d23e4ed36
Prajapati Raman <115515159+Raman01010010@users.noreply.github.com>
2024-04-04 11:02:16 +0530

    Update allowedOrigin.js

--- server/config/allowedOrigin.js
+++ server/config/allowedOrigin.js
@@ -5,14 +5,6 @@ const allowedOrigin=[
     'http://localhost:3001',
     'http://172.29.50.69:3000',
     'http://localhost:3000',
-    'http://localhost:3001',
-    'http://localhost:3002',
-    'http://172.29.50.69:3500',
-    'http://127.0.0.1:3000',
-    'https://www.google.com',
-    'careersync.free.nf',
-    'http://careersync.free.nf',
-    'http://socketrmn1.epizy.com',
     'https://source-scroll.netlify.app'
 ]
 module.exports=allowedOrigin
